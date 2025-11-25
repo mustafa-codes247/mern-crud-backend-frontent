7aaa33c2ba08be2bc60eca816ab04f5f78578174
Mustafa Altaf <usermustafa500@gmail.com>
2025-11-25 17:34:14 +0500

    backend first files added

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import routers from "./routes/userRoutes.js";
+import cors from "cors";
+import mongoose from "mongoose";
+const app= express();
+
+app.use(express.json());
+app.use(cors());
+const PORT = 5000;
+
+app.use('/api',routers);
+mongoose.connect("mongodb://localhost:27017/crud")
+.then((res)=>console.log('connected'));
+
+
+app.listen(PORT,()=>{
+    console.log(`server listen on http://localhost:${PORT}`);
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Users from './components/Users'
+import AddNewUser from './components/AddNewUser'
+import UpdateUser from './components/UpdateUser'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <h1>react app working
-      </h1>
+      <Routes>
+        <Route path="/" element={<Users/>}></Route>
+         <Route path="/add" element={<AddNewUser/>}></Route>
+          <Route path="/update" element={<UpdateUser/>}></Route>
+      </Routes>
     </div>
   )
 }
